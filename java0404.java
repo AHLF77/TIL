5b57d34f15f89a9be571458eefdfff334076c652
adhl77 <hgj2482@naver.com>
2022-04-10 20:16:24 +0900

    java0404

---
+++ java0404.java
@@ -0,0 +1,12 @@
+public class java0404 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int result = a * b;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println("result: "+result);
+	}
+
+}
